f233ec146df92186b0bf940d2ee7579e162aea7c
Alessio <alessio080103@gmail.com.com>
2022-02-01 12:55:22 +0100

    1° commit

--- app/src/main/java/com/example/leggiscrivifile/MainActivity.java
+++ app/src/main/java/com/example/leggiscrivifile/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView txtNomeFile;
     Button btnLeggi, btnScrivi;
+    Gestore gest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
